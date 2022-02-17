82544e2dcc08e67aaa46039b83e40881e4828c7b
sashamatviiuk <matviyuks23@gmail.com>
2022-02-17 14:38:55 +0300

    step 3

--- another.py
+++ another.py
@@ -2,16 +2,15 @@ from preprocess import *
 import warnings
 warnings.filterwarnings('ignore')
 
-
-
 df1 = pd.read_csv('data/alive.csv', delimiter=',')
 df2 = pd.read_csv('data/bot.csv', delimiter=',')
 
 features = ['value', 'duration']
 cat_feature='event_type'
 
-example = Prep(df1, df2, features=features, cat_feature=cat_feature)
+example = Preparation(df1, df2, features=features, cat_feature=cat_feature)
 boot_bot = example.bootstrap()
 
-example2 = Prep(df1=df1, df2=boot_bot, features=features, cat_feature=cat_feature)
-example2.plot_graph()
+example2 = Preparation(df1=df1, df2=boot_bot, features=features, cat_feature=cat_feature)
+example2.plot_graph(type_graph='hist', save=False, name_fig='graph')
+
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,8 @@
 from functions import *
-#from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-class Prep():
+class Preparation():
     # Prepare the data
     # df1 - DataFrame for alive data
     # df2 - DataFrame for bot data
@@ -20,3 +20,23 @@ class Prep():
 
     def plot_graph(self, type_graph='distplot', save=True, name_fig='graph'):
         return graph(df1=self.df1, df2=self.df2, cols=self.features, type_graph=type_graph, save=save, name_fig=name_fig)
+
+    def create_df(self, name='class'):
+        self.df1[name] = 1
+        self.df2[name] = 0
+        df = pd.concat([self.df1, self.df2])
+        X = df.loc[:, self.features.append(self.cat_feature)]
+        y = df[name]
+        return df, X, y
+
+    def train_test(self):
+        _, X, y = self.create_df()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+        return X_train, X_test, y_train, y_test
+
+    def scalar(self):
+        sc = StandardScaler()
+        X_train, X_test, _, _ = self.train_test()
+        X_train_pre = sc.fit_transform(X_train)
+        X_test_pre = sc.transform(X_test)
+        return X_train_pre, X_test_pre
